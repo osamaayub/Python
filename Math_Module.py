47b68be7a04f939ead70fb803c82634f370734f5
Osama ayub <77015981+osamaayub@users.noreply.github.com>
2021-09-01 00:00:58 +0500

    Add files via upload
    if u want to preform mathematical functions use import Math Module

---
+++ Math_Module.py
@@ -0,0 +1,26 @@
+#print the highest value
+print(max(1,2,5))
+# prints the lowest value
+print(min(12,5,-1))
+# abs returns the positive Integer value
+x=abs(-567)
+print(x)
+# power function
+
+result=pow(4,7)
+print(result)
+
+import math
+x=math.cos(0)
+print(x)
+
+x=math.sqrt(7)
+print(x)
+
+x=math.ceil(2.5)
+print(x)
+x=math.floor(2.5)
+#returns the constant value of pi
+print(x)
+x=math.pi
+print(x)
